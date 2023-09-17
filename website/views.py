4ea37ba9c669e750bba399aeb74d0e53fc0d58a2
sategf <sategf@farmingdale.edu>
2023-09-17 19:39:19 -0400

    first commit

---
+++ website/views.py
@@ -0,0 +1,47 @@
+#Main Views and URL endpoints to frontend of website
+
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Task
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+@views.route('/tasks', methods=['GET', 'POST'])
+@login_required
+def tasks():
+    if request.method == 'POST': 
+        task = request.form.get('task')#Gets the task from the HTML 
+
+        if len(task) < 1:
+            flash('Task is too short!', category='error') 
+        else:
+            new_task = Task(data=task, user_id=current_user.id)  #providing the schema for the task 
+            db.session.add(new_task) #adding the task to the database 
+            db.session.commit()
+            flash('Task added!', category='success')
+
+    return render_template("tasks.html", user=current_user)
+
+@views.route('/journal')
+@login_required
+def journal():
+    return "<h1>Future Journaling Page</h1>" 
+
+@views.route('/delete-task', methods=['POST'])
+def delete_task():  
+    task = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    taskId = task['taskId']
+    task = Task.query.get(taskId)
+    if task:
+        if task.user_id == current_user.id:
+            db.session.delete(task)
+            db.session.commit()
+
+    return jsonify({})
