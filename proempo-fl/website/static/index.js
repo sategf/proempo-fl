2eecb0006f969f99f93fe0061b90e143210471ba
sategf <123583954+sategf@users.noreply.github.com>
2023-09-18 21:38:24 -0400

    Delete proempo-fl directory

--- proempo-fl/website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteTask(taskId) {
-  fetch("/delete-task", {
-    method: "POST",
-    body: JSON.stringify({ taskId: taskId }),
-  }).then((_res) => {
-    window.location.href = "/tasks";
-  });
-}
