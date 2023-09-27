18d96b84c64b6a63da78b452a9ec2c6b226e8dc6
sategf <sategf@farmingdale.edu>
2023-09-27 19:50:39 -0400

    Merge branch 'main' of https://github.com/sategf/proempo-fl

--- install_dep.bat
+++ install_dep.bat
@@ -1,1 +1,1 @@
-pip install flask flask-login flask-sqlalchemy pytz
+pip install flask flask-login flask-sqlalchemy pytz quote flask_mail
--- website/views.py
+++ website/views.py
@@ -1,13 +1,44 @@
 #Main Views and URL endpoints to frontend of website
-
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Flask
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
+from flask_mail import Mail, Message
 from flask_login import login_required, current_user
 from .models import Task, FinishedTask
 from . import db
 from quote import quote
-import json
+import json, os
 
 views = Blueprint('views', __name__)
+
+
+app = Flask(__name__)
+
+
+# Get the values of the environment variables
+mail_username = os.environ.get('MAIL_USERNAME')
+mail_password = os.environ.get('MAIL_PASSWORD')
+
+
+
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # SMTP server
+app.config['MAIL_PORT'] = 587  #the SMTP port
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+
+# Creating the Mail instance
+mail = Mail(app)
+
+
+# Defining the support email address
+support_email = "Proempohelpdesk@gmail.com" 
+
+
+
 
 @views.route('/')
 @login_required
@@ -26,16 +57,26 @@ def generate_quote():
 def help():
     return render_template("help.html", user=current_user)
 
+
+@views.route('/pomodoro')
+@login_required
+def pomodoro():
+    task_id = request.args.get('taskId')
+    task_data = request.args.get('taskData')
+    uncompleted_tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return render_template('pomodoro.html', task_id=task_id, task_data=task_data, user=current_user, uncompleted_tasks=uncompleted_tasks)
+
+
 @views.route('/FAQ')
 @login_required
 def faq():
     accordion_items = [
-        {"id": "section1", "title": "How do I get started with the website?", "content": "You can get started by signing up through the blue link below."},
-        {"id": "section2", "title": "Is there a tutorial or onboarding process to help me navigate the application for the first time?", "content": "Sorry, the website doesn't offer any process at the moment, come back for future updates."},
-        {"id": "section3", "title": "Can I use the website without creating an account or logging in?", "content": "No, you need to create an account or log in in order to access the website."},
-        {"id": "section4", "title": "What is the pricing model for your website? Are there any free or trial versions available?", "content": "This website is free, there is no pricing model nor any trial versions."},
-        {"id": "section5", "title": "What is the pomodoro technique? How can i use it on the website?", "content": "The pomodoro method is a time management method based on 25-minute stretches of focused work broken by five-minute breaks. You can use the technique located in the Self-Help page."},
-        {"id": "section6", "title": "Where can i go to contact support if any of the issues listed above aren't available?", "content": "You can visit the Support page in the navigation bar and list your issues there."},
+        {"id": "section1", "title": "Is there a tutorial or onboarding process to help me navigate the application for the first time?", "content": "Sorry, the website doesn't offer any process at the moment, come back for future updates."},
+        {"id": "section2", "title": "Can I use the website without creating an account or logging in?", "content": "No, you need to create an account or log in in order to access the website."},
+        {"id": "section3", "title": "What is the pricing model for your website? Are there any free or trial versions available?", "content": "This website is free, there is no pricing model nor any trial versions."},
+        {"id": "section4", "title": "What is the pomodoro technique? How can I use it on the website?", "content": "The pomodoro technique is a time management method based on 25-minute stretches of focused work broken by five-minute breaks. You can use the technique located in the Self-Help page."},
+        {"id": "section5", "title": "What is the rubber duck method? How can I use it on the website?", "content": "The rubber duck method is a problem-solving technique commonly used in the programming field. Even though this method is not directly linked to mental health, it does have potential indirect benefits for mental well-being in the context of problem-solving and reducing frustration and stress. You can find this method located in the Self-Help page."},
+        {"id": "section6", "title": "Where can I go to contact support if any of the issues listed above aren't available?", "content": "You can visit the Support page in the navigation bar and list your issues there."},
        
     ]
     return render_template("FAQ.html", user=current_user, accordion_items=accordion_items)
@@ -139,10 +180,50 @@ def unMark_task():
 def about():
     return render_template("About.html", user=current_user)
 
-@views.route('/Support')
+@views.route('/Support', methods=['GET'])
 @login_required
 def support():
     return render_template("Support.html", user=current_user)
+
+@app.route('/submit_support', methods=['POST']) #something about routes isn't submitting the form through here, will be fixed soon
+def submit_support():
+    if request.method == 'POST':
+        # Get form data
+        title = request.form['title']
+        username = request.form['username']
+        issue_title = request.form['issue_title']
+        description = request.form['description']
+        
+        # Send an email to the support team with the form data
+        send_support_email(title, username, issue_title, description)
+        
+        # You can also save the form data to a database or perform other actions as needed
+        
+        # Redirect back to the support page or a thank you page
+        return redirect(url_for('thank_you'))
+
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html') # thank you message for the user after submitting form
+
+
+
+
+def send_support_email(title, username, issue_title, description):
+    # Create a message object for the email
+    msg = Message('Support Request: ' + issue_title, sender=support_email, recipients=[support_email])
+    
+    # Customize the email content with the form data
+    msg.body = f"Title: {title}\nUsername: {username}\nIssue Title: {issue_title}\nDescription: {description}"
+    
+    # Send the email
+    try:
+        mail.send(msg)
+        print("Support email sent successfully")
+    except Exception as e:
+        print(f"Error sending support email: {str(e)}")
+
 
 @views.route('/Flashcards')
 @login_required
